113cd5543f581d36693031e4ade527d7ad9778ec
Omar Elsayed <omar.elsayed@munnich.it>
2025-05-13 09:00:36 +0400

    add more to delete and update add  maint.py to the app pacage

--- app/main.py
+++ app/main.py
@@ -18,6 +18,11 @@ def find_post(id):
     for post in my_posts:
         if post["id"] == id:
             return post
+        
+def find_index_post(id):
+    for  i,post in enumerate(my_posts):
+        if post['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -48,3 +53,21 @@ def get_post(id: int):
                             detail=f"post eith id: {id} was not found")
     return {"post_details": post} 
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f" post with id {id} dose not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_post(id: int,post:Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f" post with id {id} dose not exist")
+    
+    post_dict = post.dict()
+    post_dict['id']=id
+    my_posts[index]=post_dict
+    return {"data":"my_posts"}
