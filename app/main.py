770ae69087cd190bca55dcd4ee3b001668eca31a
Omar Elsayed <omar.elsayed@munnich.it>
2025-05-20 14:00:31 +0400

    adding DB model

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response,status,HTTPException
+from fastapi import FastAPI, Response,status,HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -6,6 +6,12 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 class Post(BaseModel):
@@ -48,6 +54,10 @@ def find_index_post(id):
 def root():
     return{"message":"Hello world!!! "}
 
+@app.get("/sqlalchemy")
+def tet_post(db: Session = Depends(get_db)):
+    return{"status":"success"}
+
 @app.get("/posts")
 def get_posts():
     cursor.execute("""SELECT * FROM posts """)
@@ -78,20 +88,19 @@ def get_post(id: int):
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     cursor.execute("""DELETE FROM posts WHERE id = %s  RETURNING *""",(str(id),))
-    delete_post = cursor.fetchone()
+    deleted_post = cursor.fetchone()
     conn.commit()
-    if delete_post == None:
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f" post with id {id} dose not exist")
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.put("/posts/{id}")
 def update_post(id: int,post:Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s  RETURNING *""",(post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f" post with id {id} dose not exist")
-    
-    post_dict = post.dict()
-    post_dict['id']=id
-    my_posts[index]=post_dict
-    return {"data":"my_posts"}
+
+    return {"data":updated_post}
