5a41a1a1495e15a0d655f3e15c059abce5044900
Omar Elsayed <omar.elsayed@munnich.it>
2025-05-26 15:01:26 +0400

    add user authorization

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-
+from typing import Optional
 class PostBase(BaseModel):
     title: str
     content: str
@@ -18,3 +18,23 @@ class Post(PostBase):
 class UserCreated(BaseModel):
     email:EmailStr
     password:str
+class Userout(BaseModel):
+    id:int
+    email:EmailStr
+    created_at:datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email : EmailStr
+    password :str
+    
+class Token(BaseModel):
+    access_token : str
+    token_type :str
+    
+
+class TokenData(BaseModel):
+    id: Optional[str]
